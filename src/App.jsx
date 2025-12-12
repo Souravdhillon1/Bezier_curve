1323e4e273d05d12855863491045da4a83c45f67
Sourav Dhillon <souravdhillon50@gmail.com>
2025-12-12 17:58:55 +0530

    Initial commit: Bezier rope with physics

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import BezierRope from './BezierRope'
+
+function App() {
+
+  return (
+    <>
+    <BezierRope/>
+    </>
+  )
+}
+
+export default App
